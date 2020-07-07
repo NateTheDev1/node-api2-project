5265ef9b617b123358c357fa9e867ca17c38ea26
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-07 13:24:52 -0400

    MVP Complete

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 // const { insert } = require("../data/db");
 const db = require("../data/db");
+const { restart } = require("nodemon");
 
 router.post("/", async (req, res) => {
   const { title, contents } = req.body;
@@ -46,6 +47,80 @@ router.post("/:id/comments", async (req, res) => {
   }
 
   res.status(201).json({ ...savedComment, ...req.body });
+});
+
+router.get("/", async (req, res) => {
+  const posts = await db.find();
+  if (!posts) {
+    return res
+      .status(500)
+      .json({ error: "The posts information could not be retrieved." });
+  }
+
+  res.status(200).json({ ...posts });
+});
+
+router.get("/:id", async (req, res) => {
+  const foundPost = await db.findById(req.params.id);
+  if (foundPost.length < 1) {
+    return res
+      .status(400)
+      .json({ message: "The post with the specified ID does not exist." });
+  }
+  res.json({ ...foundPost });
+});
+
+router.get("/:id/comments", async (req, res) => {
+  const foundPost = await db.findPostComments(req.params.id);
+  if (foundPost.length < 1) {
+    return res
+      .status(400)
+      .json({ message: "The post with the specified ID does not exist." });
+  }
+  res.json({ ...foundPost });
+});
+
+router.delete("/:id", async (req, res) => {
+  const toDelete = await db.findById(req.params.id);
+  const deleted = await db.remove(req.params.id);
+  if (deleted < 1) {
+    return res
+      .status(404)
+      .json({ message: "The post with the specified ID does not exist." });
+  }
+  res.json({
+    message: `${deleted} post/posts was deleted`,
+    deleted: { ...toDelete },
+  });
+});
+
+router.put("/:id", async (req, res) => {
+  const foundPost = await db.findById(req.params.id);
+  if (foundPost.length < 1) {
+    return res
+      .status(400)
+      .json({ message: "The post with the specified ID does not exist." });
+  }
+  const { title, contents } = req.body;
+  if (title === undefined || contents === undefined) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  }
+
+  const updated = await db.update(req.params.id, { ...req.body });
+  const updatedPost = await db.findById(req.params.id);
+  if (updated < 1) {
+    return res
+      .status(500)
+      .json({ error: "The post information could not be modified." });
+  }
+  res
+    .status(200)
+    .json({
+      message: `${updated} post was modified`,
+      updated: { ...updatedPost },
+    });
 });
 
 module.exports = router;
